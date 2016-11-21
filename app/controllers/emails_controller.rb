ee760e0d6b0113f4d966fc9979d146a5a1cff8ef
sid <siddhartha@happ.co.in>
2016-11-21 15:37:34 +0530

    mail fetcher complete

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,13 +2,14 @@ class EmailsController < ApplicationController
   def index
   	mailer = MailGetter.last
   	Mail.defaults do
-  	  retriever_method :pop3, :address    => mailer.address,
+  	  retriever_method mailer.method.to_sym, :address    => mailer.address,
   	                          :port       => mailer.port,
   	                          :user_name  => mailer.user_name,
   	                          :password   => mailer.password,
   	                          :enable_ssl => true
   	end
-  	@emails = Mail.first
+  	binding.pry
+  	@emails = Mail.find(:what => :last, count: 30, order: :asc)
 
   end
 
